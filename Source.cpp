6e5e17628d01f107a5197df9ce506d529263e931
agsagds <agsagds@gmail.com>
2020-03-26 18:51:04 +0600

    Add autoselect between addition and multiply

--- Source.cpp
+++ Source.cpp
@@ -3,7 +3,11 @@ using namespace std;
 int main()
 {
     cout<<"Hello world!";
+    char c;
     int a, b;
-    cin>>a>>b;
-    cout<<a+b;
+    cin>>a>>c>>b;
+    if(c=='+')
+        cout<<a+b;
+    else if(c=='*')
+        cout<<a*b;
 }
